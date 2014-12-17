0bdae4ee77068ee6d5bbd149a747849df2ab6489
John Crocker <johnniecrocker@yahoo.com>
2014-12-17 14:05:50 -0500

    working on displaying projects

--- app/scripts/controllers/portfolio_controller.js
+++ app/scripts/controllers/portfolio_controller.js
@@ -6,6 +6,23 @@
 
     console.log('portfolio controller');
 
+    var git_repo = 'https://api.github.com/users/gt7348b/repos',
+        render_repo;
+
+    render_repo = _.template($('#respositories').html());
+
+    $.getJSON(git_repo).done( function(repo_data){
+
+      repo_data.forEach(function(repo){
+
+        console.log(repo.name);
+
+      });
+
+    });
+
+
+
   }])
 
 
