38da847766d300321842ac3cb27513da3deaad35
John Crocker <johnniecrocker@yahoo.com>
2014-12-17 18:37:39 -0500

    working on profile and calling github api

--- app/scripts/controllers/portfolio_controller.js
+++ app/scripts/controllers/portfolio_controller.js
@@ -2,26 +2,28 @@
 
 
   angular.module('Portfolio')
-  .controller('PortfolioController', ['$scope', function($scope){
+  .controller('PortfolioController', ['$scope', '$resource', function($scope, $resource){
 
     console.log('portfolio controller');
 
     var git_repo = 'https://api.github.com/users/gt7348b/repos',
-        render_repo;
-
-    render_repo = _.template($('#respositories').html());
+        render_repo,
+        projects = [];
 
     $.getJSON(git_repo).done( function(repo_data){
 
       repo_data.forEach(function(repo){
 
         console.log(repo.name);
+        projects.push(repo)
 
       });
 
-    });
+    }).then(function(){
 
+    console.log(projects);
 
+  });
 
   }])
 
