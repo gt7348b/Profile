1e45998537a7002c0fe7df96f8e3800630e64338
John Crocker <johnniecrocker@yahoo.com>
2014-12-27 15:13:47 -0500

    gulped

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -30,3 +30,51 @@ console.log('Roscoe plays peekaboo');
     })
 
 }());
+
+(function(){
+
+  angular.module('Portfolio')
+    .controller('MainController', ['$scope', function($scope){
+
+    }])
+
+}());
+
+(function(){}(
+
+
+  angular.module('Portfolio')
+  .controller('PortfolioController', ['$scope', '$resource', function($scope, $resource){
+
+    console.log('portfolio controller');
+
+    var git_repo = 'https://api.github.com/users/gt7348b/repos',
+        render_repo,
+        projects = [];
+
+    $.getJSON(git_repo).done( function(repo_data){
+
+      repo_data.forEach(function(repo){
+
+        console.log(repo.name);
+        projects.push(repo)
+
+      });
+
+    }).then(function(){
+
+    console.log(projects);
+
+  });
+
+  }])
+
+
+
+));
+
+(function(){
+
+
+
+}());
